de62c34ef8ef6bf424bb6f9bb188d1ab7b7ebbeb
Max Putilin <maxim.putilin@outlook.com>
2023-06-15 03:09:34 +0400

    AWS credentials variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,16 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "aws_access_key" {
+  description = "AWS access key ID"
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret access key"
+  default     = ""
+}
+
 variable "name" {
   type    = string
   default = "intuitive-cloud-lab"
